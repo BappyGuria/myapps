612a394414d1ff08ac1f7ae9c5307299772faa4f
BappyGuria <guriabaappye8@gmail.com>
2024-04-15 11:31:12 +0600

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -23,43 +23,33 @@ class HomePaje extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('MY APP')),
+        title: const Center(child: Text('MY APP')),
         backgroundColor: Colors.pinkAccent,
         foregroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {},
-          icon: Icon(Icons.home),
+          icon: const Icon(Icons.home),
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.notification_add))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notification_add))
         ],
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.network(
-                'https://ssl.gstatic.com/onebox/media/sports/logos/mcpMspef1hwHwi9qrfp4YQ_96x96.png'),
-            SizedBox(
-              height: 30,
+            GestureDetector(
+              onTap: (){
+                print('On Tap');
+              },
+              onDoubleTap: (){
+                print('On Double Tap');
+              },
+              onLongPress: (){
+                print('Long Prese');
+              },
+              child: Icon(Icons.add),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pinkAccent,
-                  foregroundColor: Colors.white,
-                  elevation: 5,
-                  shape: const RoundedRectangleBorder(
-                    side: BorderSide(
-                      width: 3,
-                      color: Colors.green,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(10))
-                  )),
-              child: const Text('CLICK ME'),
-            )
           ],
         ),
       ),
