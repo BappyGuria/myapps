f6361989ce52f65d873437dfd4a1e957dda79e5b
BappyGuria <guriabaappye8@gmail.com>
2024-05-01 19:55:14 +0600

    ThameData

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import 'HomeScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,46 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Image App',
       home:HomeScreen(),
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.pinkAccent,
-            foregroundColor: Colors.white,
-            padding: EdgeInsets.all(10)
-          )
-        )
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
-      themeMode: ThemeMode.light,
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MY APP'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: (){}, child: Text('Elevate Button'),style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,foregroundColor: Colors.white
-            ),),
-            ElevatedButton(onPressed: (){}, child: Text('Elevate Button')),
-            ElevatedButton(onPressed: (){}, child: Text('Elevate Button')),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 
 
