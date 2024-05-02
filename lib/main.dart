99334c88530b85160a5127362dabc4afe09fb074
BappyGuria <guriabaappye8@gmail.com>
2024-05-02 16:32:23 +0600

    ThameData

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-import 'HomeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,19 +9,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Image App',
-      home:HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size);
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).orientation);
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          backgroundColor: Colors.pinkAccent,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: const Column(
+          children: [
+            Wrap(
 
-
-// /// All Style Apply
-// class ButtonStyles{
-//   static ButtonStyle elevatStyle = ElevatedButton.styleFrom(
-// backgroundColor:  Colors.blue,
-//     foregroundColor: Colors.white
-//   );
-// }
+              children: [
+                Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Loremdfbdbdb'),
+                Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum')
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
